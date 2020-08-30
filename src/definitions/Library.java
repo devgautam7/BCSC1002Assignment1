1efe7d0217fdf53e32f2cca8bab99aa6f5a4b41f
devgautam7 <68607298+devgautam7@users.noreply.github.com>
2020-08-30 22:29:17 +0530

    Update Library.java
    Constructor

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -5,6 +5,21 @@
  *  File Name : Library.java
  * */
 package definitions;
-
+import java.util.Arrays;
 public class Library {
-}
+
+private static final int MAXIMUM_BOOKS_IN_LIBRARY = 5;
+    private Book[] booksThatAreCurrentlyAvailable;
+
+    // Parameterized Constructor
+    public Library(Book[] booksThatAreCurrentlyAvailable) {
+        this.booksThatAreCurrentlyAvailable = booksThatAreCurrentlyAvailable;
+    }
+
+    // Non-Parameterized Constructor
+    public Library() {
+        this.booksThatAreCurrentlyAvailable = new Book[MAXIMUM_BOOKS_IN_LIBRARY];
+        for (int libraryIndex = 0; libraryIndex < MAXIMUM_BOOKS_IN_LIBRARY; libraryIndex++) {
+            this.booksThatAreCurrentlyAvailable[libraryIndex] = new Book();
+        }
+    }
