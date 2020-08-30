372eb82f1f919e22c1fbe6f98ccc8bcfc362470e
devgautam7 <68607298+devgautam7@users.noreply.github.com>
2020-08-30 22:21:00 +0530

    Update Book.java
    Parameterized and non parameterized contrustor

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,26 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
+ *  User: Dev Gautam
+ *  Date: 29/08/20
  *  Time: 3:49 PM
  *  File Name : Book.java
  * */
 package definitions;
-
+import java.util.Objects;
 public class Book {
-}
+private String nameOfTheBook;
+    private String nameOfTheAuthorOfTheBook;
+    private String thirteenDigitISBNNumberOfTheBook;
+
+    // Parameterized Constructor
+    public Book(String nameOfTheBook, String nameOfTheAuthorOfTheBook, String thirteenDigitISBNNumberOfTheBook) {
+        this.nameOfTheBook = nameOfTheBook;
+        this.nameOfTheAuthorOfTheBook = nameOfTheAuthorOfTheBook;
+        this.thirteenDigitISBNNumberOfTheBook = thirteenDigitISBNNumberOfTheBook;
+    }
+
+    // Non-Parameterized Constructor
+    public Book() {
+        this.nameOfTheBook = null;
+        this.nameOfTheAuthorOfTheBook = null;
+        this.thirteenDigitISBNNumberOfTheBook = null;
+    }
