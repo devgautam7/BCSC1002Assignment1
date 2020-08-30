04bd0808fc234b355b4b2170df583a4266f76cf4
devgautam7 <68607298+devgautam7@users.noreply.github.com>
2020-08-30 22:52:37 +0530

    Update FrontDesk.java
    Class

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,13 +1,18 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Dev Gautam
+ *  Date: 30/08/20
+ *  Time: 10:51 PM
  *  File Name : FrontDesk.java
  * */
 package execution;
 
-public class FrontDesk {
-    public static void main(String[] args) {
+import definitions.Library;
+import definitions.Student;
 
-    }
-}
+import java.util.Scanner;
+
+public class FrontDesk {
+    private static final int ISSUE_A_NEW_BOOK = 1;
+    private static final int RETURN_A_PREVIOUSLY_ISSUED_BOOK = 2;
+    private static final int SHOW_ALL_ISSUED_BOOKS = 3;
+    private static final int EXIT = 4;
