815b0424c5543f0244afb829b3105938a5ab1771
devgautam7 <68607298+devgautam7@users.noreply.github.com>
2020-08-30 22:49:06 +0530

    Update Student.java
    Issue book

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -91,3 +91,14 @@ public String getNameOfTheStudent() {
         result = 31 * result + Arrays.hashCode(getNamesOfTheBooksIssuedByTheStudent());
         return result;
     }
+/**
+     * This method will issue the books to the Student
+     *
+     * @param bookIssuingIndex The index where the book is issueing.
+     * @param bookName         The name of the book which is issuing.
+     */
+    public void issueBooksToStudents(int bookIssuingIndex, String bookName) {
+        namesOfTheBooksIssuedByTheStudent[bookIssuingIndex].setNameOfTheBook(bookName);
+    }
+
+    /**
